e6321d1b99cf83f67f4f7a21f747669d310df62b
Ozziur <casaruizzo@yahoo.it>
2025-09-08 19:24:54 +0200

    Refactor Grace.c to improve code structure and maintain self-replicating functionality

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#define MAIN() int main(void){FILE *dest_file=fopen("Grace_kid.c", "w");char code[] = "#include <stdio.h>%c#define MAIN() int main(void){FILE *dest_file=fopen(%cGrace_kid.c%c, %cw%c);char code[] = %c%s%c;if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10);fclose(dest_file);return(0);}%cMAIN()%c/*%c	aaaargh%c*/";if(dest_file == NULL){return(1);}fprintf(dest_file, code, 10, 34, 34, 34, 34, 34, code, 34, 10, 10, 10, 10);fclose(dest_file);return(0);}
-MAIN()
+#define CODE "#include <stdio.h>%c#define CODE %c%s%c%c#define FINISH return(0);%c#define MAIN()int main(void){ FILE *f = fopen(%cGrace_kid.c%c, %cw%c); if (f == NULL) return (1); fprintf(f, CODE, 10, 34, CODE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10);fclose(f);FINISH}%c%c/*%c   Aaaaaargh%c*/%c%cMAIN()%c"
+#define FINISH return(0);
+#define MAIN()int main(void){ FILE *f = fopen("Grace_kid.c", "w"); if (f == NULL) return (1); fprintf(f, CODE, 10, 34, CODE, 34, 10, 10, 34, 34, 34, 34, 10, 10, 10, 10, 10, 10, 10);fclose(f);FINISH}
+
 /*
-	aaaargh
+   Aaaaaargh
 */
+
+MAIN()
